ada38dbc940f8adff970b362b5ab367a961a84d8
Robert <Robmmar90@gmail.com>
2025-05-31 18:39:37 -0700

    added text node convert for splitting inline types

--- src/nested.py
+++ src/nested.py
@@ -1,12 +1,5 @@
 from textnode import *
 
-# def type_from_delimiter(text_type):
-#     for delim in TextType:
-#         print(text_type)
-#         print(delim.value)
-#         if delim.value == text_type:
-#             return delim
-#     return None
 
 def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
     new_nodes = []
@@ -27,13 +20,3 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: 
 
     return new_nodes
                 
-# node = TextNode("This is text with a `code block` word", TextType.TEXT)
-# new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-
-# print(new_nodes)                
-
-
-
-# if arr[0] == '' node 1 is the block
-# if arr[-1] == '' node[-2] is block
-# else node[1] is block
